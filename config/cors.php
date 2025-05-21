b56c8e16b59d472d6aa13db42c41ecb22f852b2b
Daniel <144605187+D4niel-Diaz@users.noreply.github.com>
2025-05-21 23:02:35 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,35 @@
 <?php
+
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*', 'login', 'register', 'logout'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+
+    'allowed_origins' => [
+        'https://full-stack-frontend-ten.vercel.app',
+        'http://localhost:3000',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,
+
+    'supports_credentials' => false,
+
 ];
