bf292e8b45068b219581c95b27c9e60853e7a35f
Edzell04 <gimeedzell@gmail.com>
2025-05-22 14:20:56 +0800

    commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -73,7 +73,7 @@ class AdminController extends Controller
                       
                 });
             }
-
+//
             // Sorting
             if ($request->get('sort') === 'latest') {
                 $query->latest();
